7b1596048c492a3601a4023ad54bb06c5c856be7
Anton Cheng <antonassocareer@gmail.com>
2021-06-03 20:42:56 +0800

    push

--- contracts/WETH.sol
+++ contracts/WETH.sol
@@ -1,21 +1,42 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.3.0/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20 {
 
-  constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-    
-  }
+  constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol)  {}
 
   // user burn weth and withdraw eth
   function withdraw(uint256 _amount) external {
-    uint256 temp = _amount;
+    _burn(msg.sender, _amount);
+    payable(msg.sender).transfer(_amount);
   }
 
   // user put eth, get weth out
   function deposit() payable external {
-    uint256 temp = 0;
+    _mint(msg.sender, msg.value);
   }
 }
+
+
+// //SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.0;
+
+// import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// contract WETH is ERC20 {
+
+//   constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+//   // user burn weth and withdraw eth
+//   function withdraw(uint256 _amount) external {
+    
+//   }
+
+//   // user put eth, get weth out
+//   function deposit() payable external {
+//     _mint(msg.sender, msg.value);
+//   }
+// }
