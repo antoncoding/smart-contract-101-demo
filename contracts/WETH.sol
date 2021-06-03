4e925b10e34de9d3c9d06c4893cd57646bbc260c
Anton Cheng <antonassocareer@gmail.com>
2021-06-03 16:41:46 +0800

    init commit

---
+++ contracts/WETH.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.7.0;
+
+import {ERC20} from "@openzeppelin/contracts/token/erc20/ERC20.sol";
+
+contract WETH is ERC20 {
+  function withdraw(uint256 _amount) external {
+
+  }
+
+  function deposit() {
+
+  }
+}
